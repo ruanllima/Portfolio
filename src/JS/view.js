e3cb137b91b8305e783ae211475408e26ecdf070
ruanllima <ruanlimafn@gmail.com>
2025-02-04 18:48:51 -0300

    Organization certificates in the tab

--- src/JS/view.js
+++ src/JS/view.js
@@ -1,12 +1,12 @@
 function view(id){
     const element = document.getElementById("session3");
 
-    if (document.getElementById(id).style.display == "block"){
+    if (document.getElementById(id).style.display == ""){
         document.getElementById(id).style.display = "none";
         document.body.style.overflow = 'auto';
     }
     else{
-        document.getElementById(id).style.display = "block";
+        document.getElementById(id).style.display = "";
         element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
         document.body.style.overflow = 'hidden';
       
