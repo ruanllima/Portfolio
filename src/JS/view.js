018003aa64470dfacbf2af7be70777557c3fdf8c
ruanllima <ruanlimafn@gmail.com>
2025-02-04 22:31:20 -0300

    Successfully viewing and hiding certificates

--- src/JS/view.js
+++ src/JS/view.js
@@ -13,13 +13,48 @@ function view(id){
     }
 }
 
+function view_tab(tab1, tab2, tab3){
+    const element = document.getElementById("session3");
+
+    if (document.getElementById(tab1).style.display == ""){
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+        document.body.style.overflow = 'hidden';
+
+
+        if (document.getElementById(tab2).style.display == ""){
+            document.getElementById(tab2).style.display = "none";  
+            console.log('teste3'); 
+            document.getElementById(tab3).style.display = "";
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+            document.body.style.overflow = 'hidden';
+        }
+        
+        else{
+            document.getElementById(tab1).style.display = "none";
+            console.log('teste4');
+            document.body.style.overflow = 'auto';
+        }
+       
+        
+    }
+    else{
+        console.log('teste5');
+        document.getElementById(tab1).style.display = "";
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+        document.body.style.overflow = 'hidden';
+      
+    }
+}
+
+
+
 function view_certificate(id) {
 
     const div = document.getElementById("certificados");
     const view = document.getElementById('view');
     const selected = document.getElementById(id);
     
-    document.getElementById('certificados').style.display = "none";
+    document.getElementById('certificados_list').style.display = "none";
     document.getElementById('view_container').style.display = ""; // ou ""
 
     view.src = selected.src;
@@ -28,7 +63,7 @@ function view_certificate(id) {
 }
 
 function change_certificate(id){
-    const div = document.getElementById("certificados");
+    const div = document.getElementById("certificados_list");
     const certificados = div.querySelectorAll('[id]');
     const array_cert = Array.from(certificados).map(cert => cert.id);
     const currentSrc = document.getElementById("view").src;
@@ -43,4 +78,6 @@ function change_certificate(id){
     if(id === 'right' && index < array_cert.length - 1){
         view_certificate(array_cert[index + 1]);
     }
+
 }
+
